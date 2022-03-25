ff15eda8aca223f100be714d5458c44df9f9804b
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-03-25 19:32:32 +0200

    error.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import { file } from "./lib/file.js";
 
-const petras = {
-    name: "Petras"
-}
-
-const status = await file.create('users',
- 'petras.json', petras);
+const status = await file.create('users', 'petras.json', {
+    name: 'Petras'
+});
 
 console.log(status);
-
 
 file.read();
 file.update();
